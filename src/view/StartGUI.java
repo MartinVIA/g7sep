2988b20423a850ab307155378befeb28f055a6ff
tonuvictor98-cpu <tonuvictor98@gmail.com>
2025-12-02 11:46:28 +0100

    Refactor StartGUI layout and package structure

--- src/view/StartGUI.java
+++ src/view/StartGUI.java
@@ -1,10 +1,16 @@
-package view;
-
+package model;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -25,23 +31,43 @@ public class StartGUI extends Application {
         Button villager_edit = new Button("Edit existing villager");
 
         HBox bottom_menu = new HBox();
-        bottom_menu.getChildren().addAll(villager_add, villager_edit);
+        bottom_menu.getChildren().addAll(villager_add,villager_edit);
         bottom_menu.setSpacing(10);
         bottom_menu.setPrefWidth(300);
 
         HBox nav_bar = new HBox();
-        nav_bar.getChildren().addAll(villager_menu, trade_menu, task_menu, Community_points_menu);
+        nav_bar.getChildren().addAll(villager_menu,trade_menu,task_menu,Community_points_menu);
         nav_bar.setPrefWidth(300);
 
-        GridPane root = new GridPane();
-        root.setAlignment(Pos.CENTER);
-        root.setHgap(12);
+        final Label label = new Label("Address Book");
+        label.setFont(new Font("Arial", 20));
+        TableView table = new TableView<>();
+        table.setEditable(true);
+        TableColumn firstNameCol = new TableColumn("First Name");
+        TableColumn lastNameCol = new TableColumn("Last Name");
+        TableColumn emailCol = new TableColumn("Email");
+        table.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(label, table);
+        ListView<String> listView = new ListView<>();
 
-        root.add(bottom_menu, 0, 2);
-        root.add(nav_bar, 0, 0);
+         
+        final VBox list = new VBox();
+        list.setSpacing(5);
+        list.setPadding(new Insets(10, 0, 0, 10));
+        list.getChildren().addAll(label, table);
+
+        BorderPane root = new BorderPane();
+        root.setTop(nav_bar);
+        root.setCenter(list);
+        root.setBottom(bottom_menu);
+        root.setPadding(new Insets(10));
 
         Scene scene = new Scene(root, 500, 500);
         primaryStage.setScene(scene);
+        primaryStage.setTitle("I don't wanna do this anympre send help");
         primaryStage.show();
     }
 
