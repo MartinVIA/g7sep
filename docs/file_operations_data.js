694644cfb0d5a34e139c1e3bad4f2dfbcf1a1b78
Leon210109 <144994653+Leon210109@users.noreply.github.com>
2025-12-04 10:08:52 +0100

    save and web

---
+++ docs/file_operations_data.js
@@ -0,0 +1,8 @@
+window.fileOperations = window.fileOperations || {};
+window.fileOperations.personalPoints = [
+  {"id": "1", "points": "9999999" },
+  {"id": "2", "points": "0" },
+  {"id": "3", "points": "0" },
+  {"id": "4", "points": "0" }
+];
+window.fileOperations.lastUpdated = "2025-12-04T09:06:03.950225600Z";
